aef463aceccd9945dfa5b021bf2dad38f1290118
John <johnmoreau19@gmail.com>
2024-03-31 15:23:48 -0700

    First element created

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 // Place your DOM creation code here
+
+
+function createElements() {
+
+    let elementsMain = document.createElement("main")
+    elementsMain.textContent = "Hello!!!!"
+
+    document.body.appendChild(elementsMain)
+
+
+
+}
+
+
+
+createElements()
